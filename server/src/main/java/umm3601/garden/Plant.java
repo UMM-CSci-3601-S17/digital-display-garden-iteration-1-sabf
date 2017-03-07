3c9e72b4265d3db4418204db1cb119ac50c19c46
Jeiloss <15thirty>
2017-03-06 19:13:55 -0600

    Create Bed & Plant Classes

---
+++ server/src/main/java/umm3601/garden/Plant.java
@@ -0,0 +1,19 @@
+package umm3601.garden;
+
+public class Plant {
+    String _id;
+    String plantID;
+    String commonName;
+    String cultivar;
+    String source;
+    String gardenLocation;
+    int year;
+    String pageURL;
+    String[] plantImageURLs;
+    String[] recognitions;
+
+//    metadata {
+//        Integer pageViews;
+//        rating[] ratings;
+//    }
+}
