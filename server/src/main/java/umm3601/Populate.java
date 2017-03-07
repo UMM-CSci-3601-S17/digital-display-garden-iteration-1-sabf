c949b49ee5e674067e5e6046db3498353cdaba16
Jeiloss <15thirty>
2017-03-06 21:15:30 -0600

    Adds Bed, Plant, and Populate test code. It is possible to read from .csv and import into the database.

--- server/src/main/java/umm3601/Populate.java
+++ server/src/main/java/umm3601/Populate.java
@@ -1,9 +1,88 @@
 package umm3601;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import umm3601.garden.Plant;
+
+import javax.print.Doc;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Hdlanes stuep.
  * From etitncraxg dseeird dtaa trhgouh getnareing wpeegbas
  */
 public class Populate {
 
+    final static int IGNORE_LINES = 4;
+
+    private final MongoCollection<Document> gardenCollection;
+
+    public Populate() throws IOException {
+
+        MongoClient mongoClient = new MongoClient();
+        MongoDatabase db = mongoClient.getDatabase("test");
+        gardenCollection = db.getCollection("garden");
+
+    }
+
+    public MongoCollection<Document> getGardenCollection()
+    {
+        return gardenCollection;
+    }
+
+
+    public static void main (String[] args) throws IOException
+    {
+        Populate populate = new Populate();
+        BufferedReader fileIn = new BufferedReader(new FileReader("/home/mitch809/IdeaProjects/digital-display-garden-iteration-1-sabf/server/src/main/java/umm3601/Accession list 2016 for Steves Design.csv"));
+
+        String lineContainsYear = fileIn.readLine();
+//        Pattern extractYear = Pattern.compile(".*([0-9]+).*");
+//        Matcher m = extractYear.matcher(lineContainsYear);
+//        if(m.matches())
+//        {
+//            boolean PLZ = m.matches();
+//            System.out.println(PLZ + "!");//MAKE THIS A TEST.
+//            String year = m.group();
+//            System.out.println("???" + year);
+//        }
+//        System.out.println(lineContainsYear);
+
+
+        for(int i = 0; i < IGNORE_LINES-1; i++)
+           fileIn.readLine();
+
+        String line;
+        while ((line = fileIn.readLine()) != null) { //https://www.mkyong.com/java/how-to-read-and-parse-csv-file-in-java/
+            Plant p = new Plant();
+            // use comma as separator
+            String[] plant = new String[7];
+            String[] in = line.split(",");
+            for(int i = 0; i < Math.min(in.length, plant.length); i++)
+                plant[i] = in[i];
+
+            p.setPlantID(plant[0]);
+            p.setCommonName(plant[1]);
+            p.setCultivar(plant[2]);
+            p.setSource(plant[3]);
+            //SKIP to 6
+            p.setGardenLocation(plant[6]);
+            p.setYear(2016); //TODO: REVISIT
+
+            populate.getGardenCollection().insertOne(p.toBSONDocument());
+            System.out.println(p.toBSONDocument());
+        }
+
+
+
+    }
 }
--- server/src/main/java/umm3601/garden/Plant.java
+++ server/src/main/java/umm3601/garden/Plant.java
@@ -1,19 +1,126 @@
 package umm3601.garden;
 
+import com.mongodb.util.JSON;
+import org.bson.BSON;
+import org.bson.Document;
+
 public class Plant {
+
+
     String _id;
     String plantID;
+
+
+
     String commonName;
     String cultivar;
     String source;
     String gardenLocation;
+    String plantType;
     int year;
     String pageURL;
     String[] plantImageURLs;
     String[] recognitions;
 
+    public Plant()
+    {
+        plantType = "flower"; //Unless otherwise specified
+        pageURL = "";
+    }
+
 //    metadata {
 //        Integer pageViews;
 //        rating[] ratings;
 //    }
+
+    public boolean readyForJSON()
+    {
+        return true; //TODO
+    }
+
+    public Document toBSONDocument()
+    {
+        Document out = new Document();
+        Document plantData = new Document();
+
+        plantData.append("_id", this._id);
+        plantData.append("plantID", this.plantID);
+        plantData.append("plantType", this.plantType);
+        plantData.append("commonName", this.commonName);
+        plantData.append("cultivar", this.cultivar);
+        plantData.append("source", this.source);
+        plantData.append("gardenLocation", this.gardenLocation);
+        plantData.append("year", this.year);
+        plantData.append("pageURL", this.pageURL);
+        //plantData.append("plantImageURL", new String[0]);
+        //plantData.append("recognitions", new String[0]);
+
+        out.append("plant", plantData);
+        return out;
+    }
+
+    public String getID() {
+        return _id;
+    }
+
+    public void setID(String _id) {
+        this._id = _id;
+    }
+
+    public String getPlantID() {
+        return plantID;
+    }
+
+    public void setPlantID(String plantID) {
+        this.plantID = plantID;
+    }
+
+    public String getCommonName() {
+        return commonName;
+    }
+
+    public void setCommonName(String commonName) {
+        this.commonName = commonName;
+    }
+
+    public String getCultivar() {
+        return cultivar;
+    }
+
+    public void setCultivar(String cultivar) {
+        this.cultivar = cultivar;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getGardenLocation() {
+        return gardenLocation;
+    }
+
+    public void setGardenLocation(String gardenLocation) {
+        this.gardenLocation = gardenLocation;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getPageURL() {
+        return pageURL;
+    }
+
+    public void setPageURL(String pageURL) {
+        this.pageURL = pageURL;
+    }
+
 }
